a2588c9119b92f2fc463fb242676203e040d3db9
timsidorin <timsidorin@gmail.com>
2025-07-03 19:33:48 +1000

    Продолжаем накидывать

--- client/APIclient/AdminAPIClient.py
+++ client/APIclient/AdminAPIClient.py
@@ -4,6 +4,8 @@ from client.APIclient.BaseAPI import BaseAPIClient
 class AdminAPIClient(BaseAPIClient):
     """Класс для взаимодействия с апи Админа"""
     async def check_admin_status(self, username:str)->bool:
-        response = await self._send_request(
-        )
-        return response
+        # response = await self._send_request(
+        #     "GET", "/admin/reports/daily"
+        # )
+        return False
+    # Пока false чтобы проверить логику
--- client/handlers/main.py
+++ client/handlers/main.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from client.core.config import configs
 from client.keyboards.admin_keyboard import create_admin_keyboard
-from client.keyboards.teacher_keyboard import create_teacher_keyboard
+from client.keyboards.teacher_keyboard import create_teacher_keyboard, get_my_groups
 from client.APIclient.AdminAPIClient import  AdminAPIClient
 from client.APIclient.TeacherAPIClient import  TeacherAPIClient
 router = Router()
@@ -17,8 +17,17 @@ async def cmd_start(message: Message):
     client = AdminAPIClient()
     user_id = message.from_user.id
     username = message.from_user.username
-    if client.check_admin_status(username):
+    if await client.check_admin_status(username):
         await message.answer("Привет, Админ!", reply_markup=create_admin_keyboard())
     else:
         await message.answer("Привет, Преподаватель!", reply_markup=create_teacher_keyboard())
+
+
+@router.message(lambda message: message.text == "Мои группы 📜")
+async def show_my_groups(message: Message):
+    keyboard = get_my_groups(username=message.from_user.username)
+    await message.answer(
+        text="Выберите текущую группу",
+        reply_markup=keyboard.as_markup()
+    )
 
