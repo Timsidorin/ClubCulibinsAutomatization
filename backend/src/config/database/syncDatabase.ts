9d2f7f5e3e218904d7618999e308568aba8c54c5
B2ng4 <kostyan2004tt@mail.ru>
2025-07-22 21:27:37 +1000

    db(project): убрал перезапись таблиц

--- backend/src/config/database/syncDatabase.ts
+++ backend/src/config/database/syncDatabase.ts
@@ -15,7 +15,7 @@ async function syncDatabase() {
     try {
         await sequelize.authenticate();
         console.log('Подключение к БД успешно.');
-        await sequelize.sync({force: true});
+        await sequelize.sync({force: false});
         console.log('Все модели синхронизированы.');
         await createTypesUser();
     } catch (error) {
